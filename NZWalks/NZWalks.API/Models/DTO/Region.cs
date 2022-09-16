5317bde76ff9ad7007bd647d29143122a54f7a21
subhasish <subhasispradhan1999@gmail.com>
2022-09-16 15:51:11 +0530

    Added Profiles, Created, DTO,Added Auto mapper, made code asynchronous

---
+++ NZWalks/NZWalks.API/Models/DTO/Region.cs
@@ -0,0 +1,13 @@
+﻿namespace NZWalks.API.Models.DTO
+{
+    public class Region
+    {
+        public Guid Id { get; set; }
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public double Area { get; set; }
+        public double Lat { get; set; }
+        public double Longitude { get; set; }
+        public long Population { get; set; }
+    }
+}
--- NZWalks/NZWalks.API/Program.cs
+++ NZWalks/NZWalks.API/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using NZWalks.API.Data;
+using NZWalks.API.Repositories;
 using Pomelo.EntityFrameworkCore.MySql.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -10,8 +11,15 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 string mySqlConnectionString = "server=localhost;port=3306;uid=root;pwd=root;database=nwzwalks;";
 builder.Services.AddDbContext<NZWalksDbContext>(options => options.UseMySql(mySqlConnectionString, ServerVersion.AutoDetect(mySqlConnectionString)));
+
+//this will always redirect to IregionRepo to RegionRepo
+builder.Services.AddScoped<IRegionRepository,RegionRepository>();
+
+//This is used to initialize the Automapper
+builder.Services.AddAutoMapper(typeof(Program).Assembly);
 
 var app = builder.Build();
 
